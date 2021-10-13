f82908347640c284863aa0cabe996cb9e105f8c2
Guillaume Libersat <glibersat@sigill.org>
2021-10-13 15:40:16 +0200

    Xp  - Better detail integration  - Protect views using login_required

--- ephemer/apps/experiments/views.py
+++ ephemer/apps/experiments/views.py
@@ -1,9 +1,11 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, redirect, render, reverse
 from django.views.generic import CreateView, DetailView, ListView, TemplateView
 
 from . import forms, models
 
 
+@login_required
 def experiment_list(request):
     experiments = models.Experiment.objects.all()
     return render(
@@ -13,6 +15,7 @@ def experiment_list(request):
     )
 
 
+@login_required
 def experiment_detail(request, experiment_id):
     experiment = get_object_or_404(models.Experiment, pk=experiment_id)
     return render(
@@ -22,6 +25,7 @@ def experiment_detail(request, experiment_id):
     )
 
 
+@login_required
 def experiment_create(request):
     if request.method == "POST":
         form = forms.ExperimentForm(request.POST)
@@ -39,6 +43,7 @@ def experiment_create(request):
 
 
 # Sessions
+@login_required
 def session_list(request):
     sessions = models.Session.objects.all()
     return render(
@@ -48,6 +53,7 @@ def session_list(request):
     )
 
 
+@login_required
 def session_detail(request, session_id):
     session = models.Session.objects.get(pk=session_id)
     return render(
