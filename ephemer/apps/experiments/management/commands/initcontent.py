fabce0dd4fb2ca6c907c29f8f5096d70b19b6566
Marc-Antoine Dupré <marc-antoine.dupre+git@protonmail.com>
2022-02-08 10:32:07 +0100

    Extract functions from `initcontent` admin command

--- ephemer/apps/experiments/management/commands/initcontent.py
+++ ephemer/apps/experiments/management/commands/initcontent.py
@@ -1,31 +1,11 @@
-import os
-import shutil
-
-from django.conf import settings
-from django.core.management import call_command
 from django.core.management.base import BaseCommand
 
-from ephemer.apps import experiments
+from .utils import copy_directory_content_to_media_root, load_data_from
 
 
 class Command(BaseCommand):
     help = "Installe le contenu initial de l'app experiments (notices PDF, images)"
 
     def handle(self, *args, **options):
-        media_root = settings.MEDIA_ROOT
-        app_path = os.path.split(experiments.__file__)[0]
-
-        self.stdout.write(
-            f"Installing content of 'experiments' in MEDIA_ROOT ({media_root})"
-        )
-        content_path = os.path.join(app_path, "initial_content")
-        filenames = os.listdir(content_path)
-        for f in filenames:
-            shutil.copy(os.path.join(content_path, f), media_root)
-            self.stdout.write(f"- copied content file '{f}'")
-
-        self.stdout.write("Loading initial data for the 'experiments' app")
-        call_command(
-            "loaddata", os.path.join(app_path, "initial_experiments_data.json")
-        )
-        self.stdout.write("Initial data loaded")
+        copy_directory_content_to_media_root(command=self, dirname="initial_content")
+        load_data_from(command=self, filename="initial_experiments_data.json")
---
+++ ephemer/apps/experiments/management/commands/utils.py
@@ -0,0 +1,29 @@
+import os
+import shutil
+
+from django.conf import settings
+from django.core.management import call_command
+from ephemer.apps import experiments
+
+
+def get_this_app_path():
+    return os.path.split(experiments.__file__)[0]
+
+
+def copy_directory_content_to_media_root(command, dirname):
+    media_root = settings.MEDIA_ROOT
+    content_path = os.path.join(get_this_app_path(), dirname)
+    command.stdout.write(
+        f"Installing content from {content_path} in MEDIA_ROOT ({media_root})"
+    )
+    filenames = os.listdir(content_path)
+    for f in filenames:
+        shutil.copy(os.path.join(content_path, f), media_root)
+        command.stdout.write(f"- copied content file '{f}'")
+
+
+def load_data_from(command, filename):
+    filepath = os.path.join(get_this_app_path(), filename)
+    command.stdout.write(f"Loading data from {filepath}")
+    call_command("loaddata", filepath)
+    command.stdout.write("Data loaded")
