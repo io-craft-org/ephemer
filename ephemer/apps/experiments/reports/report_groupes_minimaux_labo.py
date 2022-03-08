ef88120c7eb933293e88bbe01562572c070b6559
Marc-Antoine Dupré <marc-antoine.dupre+git@protonmail.com>
2022-03-08 12:13:02 +0100

    Ajout page résultats pour XP groupes_minimaux_labo

---
+++ ephemer/apps/experiments/reports/report_groupes_minimaux_labo.py
@@ -0,0 +1,138 @@
+from django.http import HttpResponse
+from django.shortcuts import render as django_render
+import pandas as pd
+from plotly import graph_objs as go
+
+from .base import render_graphs, Graphique
+
+
+def create_graphique_scores_moyens_endogroupe_exogroupe(
+    data: pd.DataFrame,
+) -> Graphique:
+    fig_title = "Scores moyens d’identification et d’appréciation concernant l’endogroupe et l’exogroupe"
+
+    x_labels = (
+        "Appréciation de l'endogroupe",
+        "Identification à l'endogroupe",
+        "Appréciation de l'exogroupe",
+        "Identification à l'exogroupe",
+    )
+
+    columns = (
+        "player.appreciate_picturaux",
+        "player.identify_to_picturaux",
+        "player.appreciate_experientiels",
+        "player.identify_to_experientiels",
+    )
+
+    trace = go.Bar(
+        x=x_labels,
+        y=[data[c].mean() for c in columns],
+    )
+
+    fig = go.Figure()
+    fig.add_trace(trace)
+
+    fig.update_layout(title_text=fig_title)
+
+    return Graphique(fig)
+
+
+def _create_graphique_choix_rémunération(serie, category_order, title, legend=""):
+    def create_label(value):
+        return value.replace("_", "-")
+
+    fig = go.Figure()
+    fig.add_trace(
+        trace=go.Histogram(
+            histfunc="count",
+            x=serie,
+        )
+    )
+    fig.update_xaxes(categoryorder="array", categoryarray=category_order)
+    fig.update_layout(
+        title_text=title,
+        xaxis=dict(
+            tickmode="array",
+            tickvals=[val for val in category_order],
+            ticktext=[create_label(val) for val in category_order],
+        ),
+    )
+    return Graphique(fig, legend)
+
+
+def create_graphique_choix_rémunération_matrice_1(data: pd.DataFrame) -> Graphique:
+    legend = (
+        "Chaque colonne présente la répartition de rémunération choisie. Le premier chiffre de chaque "
+        + "paire correspond à la rémunération choisie pour votre groupe et le second à la rémunération "
+        + "choisie pour l’autre groupe. Par exemple le choix de la paire 19-1 implique que vous avez "
+        + "décidé de donner une rémunération de 19 pour votre groupe et de 1 pour l’autre groupe."
+    )
+    category_order_array = [
+        "7_25",
+        "8_23",
+        "9_21",
+        "10_19",
+        "11_17",
+        "12_15",
+        "13_13",
+        "14_11",
+        "15_9",
+        "16_7",
+        "17_5",
+        "18_3",
+        "19_1",
+    ]
+    return _create_graphique_choix_rémunération(
+        serie=data["player.matrix1_response"],
+        category_order=category_order_array,
+        title="Distribution des fréquences de choix de rémunération pour la matrice 1",
+        legend=legend,
+    )
+
+
+def create_graphique_choix_rémunération_matrice_2(data: pd.DataFrame) -> Graphique:
+    legend = (
+        "Chaque colonne présente la répartition de rémunération choisie. Le premier chiffre de chaque "
+        + "paire correspond à la rémunération choisie pour votre groupe et le second à la rémunération "
+        + "choisie pour l’autre groupe. Par exemple le choix de la paire 23-29 implique que vous avez "
+        + "décidé de donner une rémunération de 23 pour votre groupe et de 29 pour l’autre groupe."
+    )
+    category_order_array = [
+        "11_5",
+        "12_7",
+        "13_9",
+        "14_11",
+        "15_13",
+        "16_15",
+        "17_17",
+        "18_19",
+        "19_21",
+        "20_23",
+        "21_25",
+        "22_27",
+        "23_29",
+    ]
+    return _create_graphique_choix_rémunération(
+        serie=data["player.matrix2_response"],
+        category_order=category_order_array,
+        title="Distribution des fréquences de choix de rémunération pour la matrice 2",
+        legend=legend,
+    )
+
+
+def render(request, session) -> HttpResponse:
+    graphs = render_graphs(
+        csv_name=session.csv,
+        graph_funcs=[
+            create_graphique_scores_moyens_endogroupe_exogroupe,
+            create_graphique_choix_rémunération_matrice_1,
+            create_graphique_choix_rémunération_matrice_2,
+        ],
+    )
+
+    return django_render(
+        request,
+        template_name="experiments/session_results.html",
+        context={"session": session, "graphs": graphs},
+    )
