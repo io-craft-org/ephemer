a0f11f315fa7df5341b076696240fbd85e78b1bb
Marc-Antoine Dupré <marc-antoine.dupre+git@protonmail.com>
2022-03-21 16:21:36 +0100

    Initialize content commands now copy content files recursively

--- ephemer/apps/experiments/management/commands/utils.py
+++ ephemer/apps/experiments/management/commands/utils.py
@@ -10,16 +10,29 @@ def get_this_app_path():
     return os.path.split(experiments.__file__)[0]
 
 
-def copy_directory_content_to_media_root(command, dirname):
-    media_root = settings.MEDIA_ROOT
-    content_path = os.path.join(get_this_app_path(), dirname)
+def copy_directory_content_to_media_root(command, dirname, media_path=""):
+    media_root = os.path.join(settings.MEDIA_ROOT, media_path)
+    if not os.path.exists(media_root):
+        os.mkdir(media_root)
+    content_path = os.path.join(get_this_app_path(), dirname, media_path)
     command.stdout.write(
         f"Installing content from {content_path} in MEDIA_ROOT ({media_root})"
     )
     filenames = os.listdir(content_path)
+    if filenames:
+        filenames.sort()
+    media_dirs = []
     for f in filenames:
-        shutil.copy(os.path.join(content_path, f), media_root)
-        command.stdout.write(f"- copied content file '{f}'")
+        full_path = os.path.join(content_path, f)
+        if os.path.isdir(full_path):
+            media_dirs.append(f)
+        else:
+            shutil.copy(full_path, media_root)
+            command.stdout.write(f"- copied content file '{f}'")
+    for media_dir in media_dirs:
+        copy_directory_content_to_media_root(
+            command, dirname, media_path=os.path.join(media_path, media_dir)
+        )
 
 
 def load_data_from(command, filename):
