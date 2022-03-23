2a605fa90f258997b7f6ed924e81b4b7730ca3a9
Marc-Antoine Dupré <marc-antoine.dupre+git@protonmail.com>
2022-03-23 17:57:59 +0100

    Delete unused view create experiment

--- ephemer/apps/experiments/views.py
+++ ephemer/apps/experiments/views.py
@@ -8,7 +8,7 @@ from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.http import FileResponse, HttpResponse, JsonResponse
-from django.shortcuts import Http404, get_object_or_404, redirect, render, reverse
+from django.shortcuts import Http404, get_object_or_404, redirect, render
 
 from . import forms, models
 from .otree import exceptions as otree_exceptions
@@ -39,23 +39,6 @@ def experiment_detail(request, experiment_id):
         request,
         template_name="experiments/experiment_detail.html",
         context={"experiment": experiment},
-    )
-
-
-@login_required
-def experiment_create(request):
-    if request.method == "POST":
-        form = forms.ExperimentForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect(reverse("experiments-experiment-list"))
-    else:
-        form = forms.ExperimentForm()
-
-    return render(
-        request,
-        template_name="experiments/experiment_create.html",
-        context={"form": form},
     )
 
 
